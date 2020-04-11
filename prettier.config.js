da50e6cbb247c9084c9449c517944daa98a6fa1c
Wade Burelbach <fafnirical@users.noreply.github.com>
2020-04-10 19:38:20 -0700

    chore: setup prettier

---
+++ prettier.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  printWidth: 80,
+  // tabWidth: 2, // Handled by EditorConfig.
+  // useTabs: false, // Handled by EditorConfig.
+  semi: true,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  jsxSingleQuote: false,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'always',
+  requirePragma: false,
+  insertPragma: false,
+  proseWrap: 'always',
+  htmlWhitespaceSensitivity: 'ignore',
+  vueIndentScriptAndStyle: false,
+  // endOfLine: 'lf', // Handled by EditorConfig.
+};
