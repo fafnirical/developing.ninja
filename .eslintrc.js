9c997aeba27437250e992aa2584d8954d23da4b9
Wade Burelbach <fafnirical@users.noreply.github.com>
2020-04-11 22:11:52 -0700

    build: setup typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,4 +6,39 @@ module.exports = {
     'plugin:prettier/recommended',
     'prettier/react',
   ],
+  overrides: [
+    {
+      files: ['**/*.ts', '**/*.tsx'],
+      extends: [
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'plugin:import/typescript',
+        'prettier/@typescript-eslint',
+      ],
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+      rules: {
+        'import/extensions': [
+          'error',
+          'ignorePackages',
+          {
+            js: 'never',
+            mjs: 'never',
+            jsx: 'never',
+            ts: 'never',
+            tsx: 'never',
+          },
+        ],
+        'react/jsx-filename-extension': [
+          'error',
+          {
+            extensions: ['.tsx'],
+          },
+        ],
+        // Use TypeScript interfaces instead of prop-types.
+        'react/prop-types': 'off',
+      },
+    },
+  ],
 };
