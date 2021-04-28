7cf71029c083df519846767d32ef0391600512e8
Wade Burelbach <fafnirical@users.noreply.github.com>
2021-04-27 23:54:59 -0700

    feat: switch back to gatsby-plugin-typegen (#412)

--- graphql.config.js
+++ graphql.config.js
@@ -1,1 +1,7 @@
-module.exports = require('./.cache/graphql.config.json');
+/**
+ * @type {import('graphql-config').IGraphQLConfig}
+ */
+module.exports = {
+  schema: './src/gatsby-schema.graphql',
+  documents: ['./src/**/*.{ts,tsx}', './src/gatsby-plugin-documents.graphql'],
+};
