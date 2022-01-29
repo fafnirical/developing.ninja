51c6e1e4a760513e1b6d4bfe5c89d1814acd1c4b
Wade Burelbach <fafnirical@users.noreply.github.com>
2022-01-29 00:24:40 -0800

    chore: standardize environment variable naming

--- sites/gatsby/gatsby-config.js
+++ sites/gatsby/gatsby-config.js
@@ -39,7 +39,7 @@ module.exports = {
       resolve: 'gatsby-source-contentful',
       options: {
         spaceId: process.env.CONTENTFUL_SPACE_ID,
-        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+        accessToken: process.env.CONTENTFUL_DELIVERY_API_TOKEN,
         useNameForId: false,
       },
     },
