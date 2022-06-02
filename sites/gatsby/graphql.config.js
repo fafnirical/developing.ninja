da2d0ee01ade617bb69f7f29148fb3d39095483f
Wade Burelbach <fafnirical@users.noreply.github.com>
2022-06-02 15:30:16 -0700

    build(gatsby): only use graphql schema if it exists
    This won't be necessary in the future when we have builds to run before linting.

--- sites/gatsby/graphql.config.js
+++ sites/gatsby/graphql.config.js
@@ -1,7 +1,11 @@
+const { existsSync } = require('fs');
+
+const gatsbySchemaPath = './src/gatsby-schema.graphql';
+
 /**
  * @type {import('graphql-config').IGraphQLConfig}
  */
 module.exports = {
-  schema: './src/gatsby-schema.graphql',
+  schema: existsSync(gatsbySchemaPath) ? gatsbySchemaPath : undefined,
   documents: ['./src/**/*.{ts,tsx}', './src/gatsby-plugin-documents.graphql'],
 };
