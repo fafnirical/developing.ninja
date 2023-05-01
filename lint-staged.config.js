fc36abae9ae231399da703a3037c1cac00aa348a
Wade Burelbach <fafnirical@users.noreply.github.com>
2023-04-30 20:05:51 -0700

    chore: lint/format package.json with sort-package-json directly

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -5,11 +5,13 @@ const prettier =
  * @type {import('@schemastore/lintstagedrc.schema').BasicConfig}
  */
 module.exports = {
-  '*, !*.{js,jsx,ts,tsx}': `${prettier} --ignore-unknown`,
+  '*, !*.{js,jsx,ts,tsx}, !package.json': `${prettier} --ignore-unknown`,
 
   '*.{js,jsx,ts,tsx}': [
     () => ['tsc --noEmit'],
     'eslint --cache --cache-location node_modules/.cache/eslint --fix',
     prettier,
   ],
+
+  'package.json': ['sort-package-json', prettier],
 };
