2d442c82d0c57e2abb725246d8a3e8d3cb9aba0f
Wade Burelbach <3250671+fafnirical@users.noreply.github.com>
2023-07-19 17:51:42 -0700

    chore(eslint): use new typescript-eslint shared configs (#1395)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,9 +10,8 @@ module.exports = {
   extends: [
     'airbnb-base',
     'plugin:@eslint-community/eslint-comments/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'plugin:@typescript-eslint/strict',
+    'plugin:@typescript-eslint/strict-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
     'plugin:import/typescript',
     'plugin:perfectionist/recommended-natural',
     'prettier',
