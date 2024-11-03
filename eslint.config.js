22a9799cff37a78fe0ed08c292795984aebf1a8a
Wade Burelbach <3250671+fafnirical@users.noreply.github.com>
2024-11-03 01:30:40 -0800

    chore(deps-dev): update ESLint configuration for ESLint v9

---
+++ eslint.config.js
@@ -0,0 +1,52 @@
+import js from '@eslint/js';
+import comments from '@eslint-community/eslint-plugin-eslint-comments/configs';
+import react from '@eslint-react/eslint-plugin';
+import gitignore from 'eslint-config-flat-gitignore';
+import prettier from 'eslint-config-prettier';
+import * as oxcResolver from 'eslint-import-resolver-oxc';
+import imports from 'eslint-plugin-import-x';
+import perfectionist from 'eslint-plugin-perfectionist';
+import { configs as typescriptConfigs } from 'typescript-eslint';
+
+/**
+ * @type {Array<
+ *   | import('eslint').Linter.Config
+ *   | import('@typescript-eslint/utils').TSESLint.FlatConfig.Config
+ * >} */
+const config = [
+  gitignore(),
+  {
+    linterOptions: {
+      reportUnusedDisableDirectives: true,
+    },
+  },
+  js.configs.recommended,
+  comments.recommended,
+  imports.flatConfigs.recommended,
+  {
+    settings: {
+      'import-x/resolver': {
+        name: 'oxc',
+        resolver: oxcResolver,
+      },
+    },
+  },
+  perfectionist.configs['recommended-natural'],
+  ...typescriptConfigs.strictTypeChecked,
+  ...typescriptConfigs.stylisticTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  imports.flatConfigs.typescript,
+  react.configs.recommended,
+  react.configs['recommended-type-checked'],
+
+  prettier,
+];
+
+export default config;
---
+++ types/src/types/eslint-community__eslint-plugin-eslint-comments.d.ts
@@ -0,0 +1,8 @@
+declare module '@eslint-community/eslint-plugin-eslint-comments/configs' {
+  import type { Linter } from 'eslint';
+
+  const configs: {
+    recommended: Linter.Config;
+  };
+  export default configs;
+}
