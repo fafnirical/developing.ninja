233c640520cbb05ad367345d983b0d7a6e4c0c84
Wade Burelbach <3250671+fafnirical@users.noreply.github.com>
2024-11-06 22:26:43 -0800

    refactor(remix): use remix-flat-routes

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { vitePlugin as remix } from '@remix-run/dev';
+import { flatRoutes } from 'remix-flat-routes';
 import { defineConfig } from 'vite';
 
 declare module '@remix-run/node' {
@@ -16,6 +17,19 @@ export default defineConfig({
         v3_relativeSplatPath: true,
         v3_singleFetch: true,
         v3_throwAbortReason: true,
+      },
+      ignoredRouteFiles: ['**/**'],
+      routes: (defineRoutes) => {
+        return flatRoutes('routes', defineRoutes, {
+          ignoredRouteFiles: [
+            '.*',
+            '**/*.css',
+            '**/*.test.{js,jsx,ts,tsx}',
+
+            '**/*.server.{js,jsx,ts,tsx}',
+            '**/*.client.{js,jsx,ts,tsx}',
+          ],
+        });
       },
     }),
   ],
